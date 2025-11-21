43ca93f1102bc05925672cc783acb3448f58ff5a
SylvannaCiss <20243ds146@utez.edu.mx>
2025-11-20 21:29:15 -0600

    Agregue MediaDao

---
+++ app/src/main/java/com/example/grabadormultimedia1/data/MediaDao.kt
@@ -0,0 +1,18 @@
+package com.example.grabadormultimedia1.data
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface MediaDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertMedia(item: MediaItem)
+
+    @Query("SELECT * FROM media_items WHERE type = :type ORDER BY date DESC")
+    fun getMediaByType(type: MediaType): Flow<List<MediaItem>>
+}
+
